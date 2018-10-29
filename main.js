f47564b5583c1ab9d4ee44c3badcaa8d5a5ff48f
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-29 08:42:26 -0600

    added template for wikepedia planets pages

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const MyComponent = React.createElement('h1', null, 'Hello World')
 
+class Planet extends React.Component {
+    render() {
+        const link = `https://en.wikipedia.org/wiki/${this.props.name}_planet)`
+
+        return React.createElement('a', { href: link, target: "blank" }, this.props.name)
+    }
+}
+
 ReactDOM.render(
     MyComponent,
     document.getElementById('app')
