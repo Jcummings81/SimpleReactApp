49ffc3c032a161633c4962bd1aa705bf6736d3ce
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-29 13:33:57 -0600

    turned planet names into array and mapped over to csimplify and abstract process

--- main.js
+++ main.js
@@ -3,24 +3,27 @@ class Planet extends React.Component {
     render() {
         const link = `https://en.wikipedia.org/wiki/${this.props.name}_planet)`
 
-        return React.createElement('a', { href: link, target: "_blank" }, this.props.name)
+        return React.createElement(
+            'a', 
+        { href: link, target: "_blank" },
+        [
+         this.props.name,
+         ' | '
+        ]
+        )
     }
 }
 
 class PlanetContainer extends React.Component {
     render() {
-        return React.createElement(
-            'div',
-            null,
-            [   "test",
-                React.createElement('br'),
-                React.createElement(Planet, {name: 'Earth'}),
-                React.createElement('br'),
-                React.createElement(Planet, {name: 'Mars'}),
-                React.createElement('br'),
-                React.createElement(Planet, {name: 'Pluto'})
-            ]
-        )
+        const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Neptune', 'Uranus', 'Pluto'].map( name => {
+            return React.createElement(Planet, { name })
+        })
+            return React.createElement(
+                'div',
+                null,
+                planets
+            )
     }
 }
 const MyComponent = React.createElement(PlanetContainer, { name: 'Mars'})
